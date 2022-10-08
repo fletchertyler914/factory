23fb2e039d429e477d6bf287cb23f80bb7fe85e2
Tyler Fletcher <fletchertyler914@yahoo.com>
2022-10-07 23:52:16 -0500

    Initial Commit

---
+++ libs/ui/src/lib/components/button/button.stories.tsx
@@ -0,0 +1,30 @@
+import { Story, Meta } from '@storybook/react';
+import { useState } from 'react';
+import { Button, ButtonProps } from './button';
+
+export default {
+  component: Button,
+  title: 'Button',
+  argTypes: {
+    onClick: { action: 'onClick executed!' },
+  },
+} as Meta;
+
+const Template: Story<ButtonProps> = (args) => {
+  const [clicked, setClicked] = useState<number>(0);
+  return (
+    <div className="bg-gray-100 p-20">
+      <Button
+        {...args}
+        onClick={() => setClicked(clicked + 1)}
+        text={args.text}
+      />
+      <div>Button has been clicked {clicked} times</div>
+    </div>
+  );
+};
+
+export const Primary = Template.bind({});
+Primary.args = {
+  text: 'Click Me',
+};
